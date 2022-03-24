2e426e511d975abef0cfed6b7fd5dec6461c5488
Rafaela de Moraes Papale <rafaela.papale@gec.inatel.br>
2022-03-23 22:16:23 -0300

    Corrigindo nome da pasta

--- Aula 6 - Códigos/server.js
+++ Aula 6 - Códigos/server.js
@@ -1,5 +1,5 @@
-require('./src/infraestructure/database');
-const app = require('./src/infraestructure/rest_server');
+require('./src/infrastructure/database');
+const app = require('./src/infrastructure/rest_server');
 
 app.listen(5000, () => {
     console.log('REST server running on port 5000...');
